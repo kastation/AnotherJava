3821231fb7ff4a3556ed6d65b9816e9046904641
kapan <kapan@DESKTOP-IOP97ID>
2016-01-15 13:13:22 -0600

    Initial commit

---
+++ src/victorJava/connectDB.java
@@ -0,0 +1,31 @@
+package victorJava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connectDB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection conn = null;
+		
+		try {
+		    conn =
+		       DriverManager.getConnection("jdbc:mysql://localhost/victor_test?" +
+		                                   "user=root&password=Interstella8");
+
+		    // Do something with the Connection
+		    System.out.println("Connected to mysql db");
+		   
+		} catch (SQLException ex) {
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		
+		
+	}
+
+}
